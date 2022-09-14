6d12023a2fbd2587781d3cf90453b5ae61eb9a23
devourmealive <foxmanlisichka@yandex.ru>
2022-09-14 16:18:06 +0300

    updated controller with offset validation and is patients found validation

--- src/main/java/ru/mis2022/controllers/registrar/RegistrarPatientRestController.java
+++ src/main/java/ru/mis2022/controllers/registrar/RegistrarPatientRestController.java
@@ -1,5 +1,8 @@
 package ru.mis2022.controllers.registrar;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.mis2022.models.dto.patient.PatientDto;
 import ru.mis2022.models.response.Response;
 import ru.mis2022.service.dto.PatientDtoService;
+import ru.mis2022.utils.validation.ApiValidationUtils;
 
 import java.util.List;
 
@@ -20,14 +24,24 @@ public class RegistrarPatientRestController {
 
     private final PatientDtoService patientDtoService;
 
+    @ApiOperation("Регистратор получает пользователей по паттерну имени, или фамилии, или полиса, или снилса," +
+            "или если все эти значения null, то регистратор получит всех пользователей")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Список найденных по паттерну пользователей"),
+            @ApiResponse(code = 404, message = "Пользователей по этому паттерну не найдено"),
+            @ApiResponse(code = 422, message = "Неверно указанная страница")
+    })
     @GetMapping
     public Response<List<PatientDto>> searchPatientByFirstNameOrLastNameOrPolisOrSnils(
             @RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName,
             @RequestParam("polis") String polis, @RequestParam("snils") String snils,
             @RequestParam("offset") Integer offset) {
-            return Response.ok(patientDtoService.findPatientsByFirstNameOrLastNameOrPolisOrSnilsPattern(
-                    firstName, lastName, polis, snils, offset
-            ));
+        ApiValidationUtils.expectedTrue(offset >= 0, 422, "Неверно указанная страница");
+        List<PatientDto> patientsDto = patientDtoService.findPatientsByFirstNameOrLastNameOrPolisOrSnilsPattern(
+                firstName, lastName, polis, snils, offset
+        );
+        ApiValidationUtils.expectedFalse(patientsDto.isEmpty(), 404, "Пользователей по этому паттерну не найдено");
+        return Response.ok(patientsDto);
     }
 
 }
